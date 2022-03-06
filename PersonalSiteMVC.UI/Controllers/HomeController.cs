7de9677c3b99a71d50fe5a7eab7a553ff9baaa86
Austin Wallace <auswallace@outlook.com>
2022-03-05 18:56:19 -0600

    Added working contact form

--- PersonalSiteMVC.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSiteMVC.UI.Models;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net;
 
 namespace PersonalSiteMVC.UI.Controllers
 {
@@ -35,9 +39,70 @@ namespace PersonalSiteMVC.UI.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have recieved an email from {cvm.Name}.<br />" +
+                $"Subject: {cvm.Subject}<br />" +
+                $"Message: {cvm.Message}<br />" +
+                $"Please respond to {cvm.Email} with your reply.";
+
+            MailMessage mm = new MailMessage(
+                
+                //FROM 
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                //TO
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+
+                //Subject
+                cvm.Subject,
+
+                //BODY
+                message
+                
+                );
+
+            mm.IsBodyHtml = true;
+
+            mm.Priority = MailPriority.High;
+
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            client.Credentials = new NetworkCredential(
+                
+                //User
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                //Password
+                ConfigurationManager.AppSettings["EmailPass"].ToString()
+                );
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry, but your request could not be completed at this time. Please try again later.<br />Error Message: {ex.StackTrace}";
+
+                return View(cvm);
+
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
